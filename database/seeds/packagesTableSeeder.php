692bd49cac94f8ebd224da6f80a3518633a8b6ad
Zanco1993 <zanconatofabio@gmail.com>
2022-03-07 16:25:48 +0100

    add faker data and es complete

--- database/seeds/packagesTableSeeder.php
+++ database/seeds/packagesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Generator as Faker;
 use App\Package;
 use Illuminate\Database\Seeder;
 
@@ -10,15 +11,31 @@ class packagesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // utilizzo inizialmente dei valori statici non creati da faker
-        // creo una riga quindi creo un'istanza del Model Package importato
-        $newPackage = new Package();
-        $newPackage->name = 'Avventura'; 
-        $newPackage->location = 'Mexico';
-        $newPackage->costs = 100;
-        $newPackage->save();
+    // public function run()
+    // {
+    //     // utilizzo inizialmente dei valori statici non creati da faker
+    //     // creo una riga quindi creo un'istanza del Model Package importato
 
+    //     $newPackage = new Package();
+    //     $newPackage->name = 'Avventura'; 
+    //     $newPackage->location = 'Mexico';
+    //     $newPackage->costs = 100;
+    //     $newPackage->save();
+
+    // }
+
+    public function run(Faker $faker)
+    {
+        // utilizzo faker
+
+        for ($i = 0; $i < 100; $i++) {
+
+            $newPackage = new Package();
+            $newPackage->name = $faker->name();
+            $newPackage->location = $faker->word();
+            $newPackage->num_people = $faker->numberBetween(1, 20);
+            $newPackage->costs = $faker->numberBetween(50, 300);
+            $newPackage->save();
+        }
     }
 }
